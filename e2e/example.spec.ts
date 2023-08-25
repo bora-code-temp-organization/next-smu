a6dfbe58453c0a4588349c614f92ae240eb2a7ec
Bora Lee <learnerbr@gmail.com>
2023-08-25 19:32:29 +0900

    타이틀 수정

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("./")
 
-  await expect(page).toHaveTitle(/Next.js Enterprise Boilerplate/)
+  await expect(page).toHaveTitle(/상명대학교 오픈소스 실습/)
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,13 @@ export default function Web() {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
-        <title>Next.js Enterprise Boilerplate</title>
+        <title>상명대학교 오픈소스 실습</title>
       </Head>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+            상명대학교 오픈소스 실습
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
